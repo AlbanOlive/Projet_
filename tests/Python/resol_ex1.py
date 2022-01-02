766b78f09a0be51657790620ec40dc591569bda5
JeremyMusset <91885777+JeremyMusset@users.noreply.github.com>
2022-01-02 18:21:50 +0100

    Update resol_ex1.py

--- tests/Python/resol_ex1.py
+++ tests/Python/resol_ex1.py
@@ -14,12 +14,10 @@ h = (T-t0) / (N+1)
 v = h*h                                                  # v = h² pour alléger les calculs
 x0 = np.zeros(N).T
 
-
 # On créer le vecteur x des points d'évaluations
 x = np.zeros(N)
 for i in range(1,N+1) : 
     x[i-1] = t0 + i*h
-
 
 # On créer la matrice A
 A = 1/v * (np.eye(N) + np.eye(N) - np.diag(np.ones(N-1),1) - np.diag(np.ones(N-1),-1))
@@ -34,8 +32,7 @@ def mat_b (t0, T, N, alpha, beta) :       # α = u(t0)       β = u(T)
     b = [0.0]*(N)
     b[0] = S(x[1]) +  alpha / v           # b[0] = S_1 + α/h²   
     for i in range (1,N-1) : 
-        b[i] =  S(x[i+1])                 # b[i] = S(t0 + h*(i+1)) = S(x_i+1) = S_i+1
-    
+        b[i] =  S(x[i+1])                 # b[i] = S(t0 + h*(i+1)) = S(x_i+1) = S_i+1    
     b[N-1] = S(T) + beta / v              # b[N-1] = S_N + β/h²
     return b
 
@@ -49,8 +46,6 @@ print(b)
 
 # On retrouve u grâce aux méthodes de Jacobi et Gauss-Seidel
 print("x =",x)
-
 print("Jacobi : u(x) = ", jacobi(A, b, x0, eps, itmax)[0])
-
 print("Gauss-Seidel : u(x) = ", gauss_seidel(A, b, x0, eps, itmax)[0])
 
